d5aef80a6b071447b325dc7e281de1af980c5f67
unknown <UFG@CC5-34.ufg.local>
2023-10-28 14:14:17 -0600

    Doc

--- src/main/java/com/mitocode/model/Camisa.java
+++ src/main/java/com/mitocode/model/Camisa.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,7 +13,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "camisa")
 public class Camisa {
     
     @Id
